4cff79ac8ef11f3831ecb418766330ee84f63fff
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-05-01 15:40:33 +0200

    Added notfound page (similar to openai)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider, RequireAuth } from 'react-auth-kit';
 import AuthPage from './pages/AuthPage';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
 import VerifyPage from './pages/VerifyPage';
 import MainPage from './pages/MainPage';
-import { AuthProvider, RequireAuth } from 'react-auth-kit';
 import ChatPage from './pages/ChatPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
     return (
@@ -23,11 +24,19 @@ const App = () => {
                                 </RequireAuth>
                             }
                         />
-                        <Route path='/c/:id' element={<ChatPage />} />
+                        <Route
+                            path='/c/:id'
+                            element={
+                                <RequireAuth loginPath='/auth'>
+                                    <ChatPage />
+                                </RequireAuth>
+                            }
+                        />
                         <Route path='/auth' element={<AuthPage />} />
                         <Route path='/auth/signup' element={<SignupPage />} />
                         <Route path='/auth/login' element={<LoginPage />} />
                         <Route path='/auth/verify/:token' element={<VerifyPage />} />
+                        <Route path='*' element={<NotFoundPage />} />
                     </Routes>
                 </Router>
             </AuthProvider>
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { Typography, Stack } from '@mui/material';
+
+const NotFoundPage = () => {
+    useEffect(() => {
+        document.title = '404: This page could not be found.';
+    }, []);
+
+    return (
+        <div id='NotFoundPage' style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Stack direction='row'>
+                <Typography variant='h5' sx={{ fontFamily: 'Noto Sans', color: 'white', borderRight: '1px solid #71727A', pr: '20px', height: '50px', lineHeight: '50px' }}>
+                    404
+                </Typography>
+                <Typography variant='body2' sx={{ fontFamily: 'Noto Sans', color: 'white', pl: '20px', height: '50px', lineHeight: '50px' }}>
+                    This page could not be found.
+                </Typography>
+            </Stack>
+        </div>
+    );
+};
+
+export default NotFoundPage;
